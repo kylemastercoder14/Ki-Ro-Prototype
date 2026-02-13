965d6d37177f057fafa7069843200df0e485a28e
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2026-02-13 19:15:50 +0800

    updated code

--- src/app/(main)/(protected)/layout.tsx
+++ src/app/(main)/(protected)/layout.tsx
@@ -1,11 +1,8 @@
 import { ReactNode } from "react";
 import { AppShell } from "@/components/custom/app-shell";
-import { RoleGuard } from "@/components/custom/role-guard";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <RoleGuard>
-      <AppShell>{children}</AppShell>
-    </RoleGuard>
+    <AppShell>{children}</AppShell>
   );
 }
